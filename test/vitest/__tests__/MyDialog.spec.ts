3f8d347f9f45567ecb1202cbcece6686eb6925e5
Stefan Schneider <stefan.schneider@gmx.net>
2022-03-26 17:11:44 +0100

    feat: :sparkles: added quasar vite plugin

--- test/vitest/__tests__/MyDialog.spec.ts
+++ test/vitest/__tests__/MyDialog.spec.ts
@@ -1,19 +1,14 @@
-import { describe, expect, test } from 'vitest';
 import { mount } from '@vue/test-utils';
-import MyDialog from '../../src/components/MyDialog.vue';
-
-import { installQuasarPlugin } from '../setup';
-
-installQuasarPlugin({});
+import MyDialog from './demo/MyDialog.vue';
 
 describe('MyDialog', () => {
   test('should mount the document body and expose for testing', () => {
+    debugger;
     const wrapper = mount(MyDialog, {
       props: {
         isDialogOpen: true,
       },
     });
-
     expect(wrapper.find('.q-dialog')?.exists()).toBeTruthy();
   });
 
--- vitest.config.js
+++ vitest.config.js
@@ -1,44 +1,26 @@
 /// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
-import { configDefaults } from 'vitest/config';
 import Vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig(() => ({
-  plugins: [Vue()],
+  plugins: [
+    Vue({ template: { transformAssetUrls } }),
+    quasar({ sassVariables: 'src/quasar-variables.sass' }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: ['vue'],
+    }),
+  ],
   test: {
     globals: true,
-    root: './',
-    include: [
-      'src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-      'test/vitest/__tests__/**/*.{test,spec}.{js,ts}?(x)',
-    ],
-    exclude: [
-      ...configDefaults.exclude,
-      'packages/template/*',
-      '**/node_modules/**',
-    ],
     environment: 'jsdom',
-    transformMode: {
-      web: [/\.[jt]sx$/, /\.vue$/],
-    },
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', 'vue'],
-    alias: {
-      '@': './src',
-      '@components': './src/components',
-      '@layouts': './src/layouts',
-      '@pages': './src/pages',
-      '@store': './src/store',
-      '@utils': './src/utils',
-      quasar: 'quasar/dist/quasar.esm.prod.js',
-      app: './',
-      src: './src',
-      '~': './',
-      layouts: './src/layouts',
-      pages: './src/pages',
-      boot: './src/boot',
-      '.*css': '@quasar/quasar-app-extension-testing-unit-jest/stub.css',
-    },
   },
 }));
